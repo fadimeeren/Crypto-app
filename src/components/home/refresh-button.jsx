502553081f940a7a8320ea7702eafd8ea25a2e37
fadimeeren <fadime_ern135@outlook.com>
2025-11-17 21:35:03 +0300

    updated

--- src/components/home/refresh-button.jsx
+++ src/components/home/refresh-button.jsx
@@ -1,6 +1,10 @@
+import { RefreshCw } from "lucide-react";
+
 const RefreshButton = () => {
   return (
-    <div>refresh-button</div>
+    <div className="p-3 bg-blue-600 text-white rounded-lg hover-bg-blue-700 disabled:opacity-50 disabled:cursor-not-allowed transition">
+      <RefreshCw className="size-5"/>
+    </div>
   )
 }
 
