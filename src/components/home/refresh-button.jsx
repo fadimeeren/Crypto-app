ab3265c25d0a4cd153827f9b90720172ef08e44e
fadimeeren <fadime_ern135@outlook.com>
2025-11-22 19:13:10 +0300

    Updated

--- src/components/home/refresh-button.jsx
+++ src/components/home/refresh-button.jsx
@@ -1,11 +1,15 @@
 import { RefreshCw } from "lucide-react";
+import React from "react";
 
-const RefreshButton = () => {
+const RefreshButton = ({ fetchCoins }) => {
   return (
-    <div className="p-3 bg-blue-600 text-white rounded-lg hover-bg-blue-700 disabled:opacity-50 disabled:cursor-not-allowed transition">
-      <RefreshCw className="size-5"/>
-    </div>
-  )
-}
+    <button
+      onClick={() => fetchCoins(true)}
+      className="p-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 disabled:opacity-50 disabled:cursor-not-allowed transition"
+    >
+      <RefreshCw className="size-5" />
+    </button>
+  );
+};
 
-export default RefreshButton;
+export default React.memo(RefreshButton);
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,35 +1,76 @@
-import { useState, useEffect } from "react";
+import { useCallback, useEffect, useState, useMemo } from "react";
+import InfoList from "../../components/home/info-list";
 import RefreshButton from "../../components/home/refresh-button";
 import Searchbar from "../../components/home/searchbar";
-import InfoList from "../../components/home/info-list";
 import coinApi from "../../services/coinApi";
 import CoinCard from "../../components/home/coin-card";
 import Loader from "../../components/loader";
+import RefreshInfo from "../../components/home/refresh-info";
+import Error from "../../components/error";
 
 const Home = () => {
   const [loading, setLoading] = useState(true);
+  const [refreshing, setRefreshing] = useState(false);
   const [error, setError] = useState(null);
   const [coins, setCoins] = useState([]);
   const [lastUpdated, setLastUpdated] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
 
   // api'dan coin verilierini çeken fonksiyon
-  const fetchCoins = () => {
-    setLoading(true);
+  const fetchCoins = useCallback((isRefreshing = false) => {
+    if (isRefreshing) {
+      setRefreshing(true);
+    } else {
+      setLoading(true);
+    }
 
     coinApi
       .getTopCoins()
       .then((data) => {
         setCoins(data);
         setLastUpdated(new Date());
+        setError(null); // api'dan veri gelince error'u temizle
       })
       .catch((err) => setError(err.message))
-      .finally(() => setLoading(false));
-  };
+      .finally(() => {
+        setLoading(false);
+        setRefreshing(false);
+      });
+  }, []);
 
   // component ekrana gelince verileri çek
   useEffect(() => {
     fetchCoins();
   }, []);
+
+  // otomatik yenilenme - her 30sn de bir
+  useEffect(() => {
+    const id = setInterval(() => {
+      fetchCoins(true);
+    }, 30000);
+
+    // component ekrandan ayrılınca intervalı temizle
+    return () => clearInterval(id);
+  }, []);
+
+  // coin veya aratılan kelime her değişitiğinde filtrele
+  const filtredCoins = useMemo(() => {
+    // bir şey aratılmadıysa filtreleme yapma
+    if (!searchTerm.trim()) return coins;
+
+    // aratılan terimi küçük harfe çevir
+    const term = searchTerm.toLowerCase();
+
+    // filtreleme yap
+    return coins.filter(
+      (coin) =>
+        coin.name.toLowerCase().includes(term) ||
+        coin.symbol.toLowerCase().includes(term)
+    );
+  }, [coins, searchTerm]);
+
+  // hata durumu
+  if (error) return <Error message={error} refetch={fetchCoins} />;
 
   return (
     <div className="space-y-6">
@@ -46,30 +87,30 @@ const Home = () => {
 
         {/* Arama ve Yenileme */}
         <div className="flex items-center gap-5">
-          <Searchbar />
+          <Searchbar onSearch={setSearchTerm} />
 
-          <RefreshButton />
+          <RefreshButton fetchCoins={fetchCoins} />
         </div>
       </div>
 
-      {/* Bilgiler*/}
-      <div>
-        <InfoList
-          total={coins.length}
-          lastUpdate={lastUpdated?.toLocaleTimeString()}
-        />
+      {/* Bilgiler */}
+      <InfoList
+        total={coins.length}
+        lastUpdate={lastUpdated?.toLocaleTimeString()}
+      />
 
-        {/* Listeleme */}
-        {loading && coins.length < 1 ? (
+      {/* Listeleme */}
+      {loading && coins.length < 1 ? (
         <Loader />
       ) : (
         <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-          {coins.map((coin) => (
+          {filtredCoins.map((coin) => (
             <CoinCard key={coin.id} coin={coin} />
           ))}
         </div>
       )}
-      </div>
+
+      <RefreshInfo show={refreshing} />
     </div>
   );
 };
